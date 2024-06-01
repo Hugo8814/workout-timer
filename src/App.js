9b4b7780402b9a4f6ac16a2bec8cb5f202275161
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-06-01 21:59:50 +0100

    ss

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+import Calculator from "./Calculator";
+import ToggleSounds from "./ToggleSounds";
+
+function App() {
+  const [allowSound, setAllowSound] = useState(true);
+  const [time, setTime] = useState(formatTime(new Date()));
+
+  // Will be be AM or PM
+  const partOfDay = useMemo(() => time.slice(-2), [time]);
+
+  const workouts = useMemo(() => {
+    return [
+      {
+        name: "Full-body workout",
+        numExercises: partOfDay === "AM" ? 9 : 8,
+      },
+      {
+        name: "Arms + Legs",
+        numExercises: 6,
+      },
+      {
+        name: "Arms only",
+        numExercises: 3,
+      },
+      {
+        name: "Legs only",
+        numExercises: 4,
+      },
+      {
+        name: "Core only",
+        numExercises: partOfDay === "AM" ? 5 : 4,
+      },
+    ];
+  }, [partOfDay]);
+
+  function formatTime(date) {
+    return new Intl.DateTimeFormat("en", {
+      month: "short",
+      year: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    }).format(date);
+  }
+
+  const tick = useCallback(() => {
+    setTime(formatTime(new Date()));
+  }, []);
+
+  useEffect(
+    function () {
+      const id = setInterval(function () {
+        tick();
+      }, 1000);
+
+      return () => clearInterval(id);
+    },
+    [tick]
+  );
+
+  return (
+    <main>
+      <h1>Workout timer</h1>
+      <time>For your workout on {time}</time>
+      <ToggleSounds allowSound={allowSound} setAllowSound={setAllowSound} />
+      <Calculator workouts={workouts} allowSound={allowSound} />
+    </main>
+  );
+}
+
+export default App;
